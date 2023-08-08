36956c310484277fb25a1a93935b7cf20d6196a2
GloriaBenedetti <141593428+GloriaBenedetti@users.noreply.github.com>
2023-08-08 11:23:08 -0300

    update

---
+++ src/purebagit/voley.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package purebagit;
+
+/**
+ *
+ * @author Glori
+ */
+public class voley {
+    private int jugadores;
+
+    public voley() {
+    }
+
+    public int getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(int jugadores) {
+        this.jugadores = jugadores;
+    }
+    
+    
+}
